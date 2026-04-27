fe72b8b899ccb0143e92c1bad5aae77adbaa7f51
Chukwudi Okechukwu <okjohn644@gmail.com>
2026-04-27 18:38:43 +0100

    added the favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "@/components/site/Navbar";
 import { Footer } from "@/components/site/Footer";
 import { FloatingWhatsApp } from "@/components/site/FloatingWhatsApp";
 import { ScrollToTop } from "@/components/site/ScrollToTop";
+import myLogo from "@/assets/favicon.png"
 
 function NotFoundComponent() {
   return (
@@ -39,6 +40,7 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
+       { rel: "icon", href: myLogo, sizes: "any"},
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@500;600;700;800&display=swap" },
         { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lobster:wght@400;500;600;700&family=Poppins:wght@500;600;700;800&display=swap" },
