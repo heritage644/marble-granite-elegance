db2f29264e81bc9d7636302de8c9bf93a83aa162
Chukwudi Okechukwu <okjohn644@gmail.com>
2026-04-27 18:44:33 +0100

    added the base path

--- src/router.tsx
+++ src/router.tsx
@@ -58,6 +58,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: {},
+    basepath: "/marble-granite-elegance",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
     defaultErrorComponent: DefaultErrorComponent,
